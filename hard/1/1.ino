bc2c5ec38ed2c3fb25f9745feb624d4feaf28752
Sabetay <lolasabetay@gmail.com>
2024-08-14 15:55:26 -0300

    declaracion de pines

---
+++ hard/1/1.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(3,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(A0,INPUT);
+pinMode(A1,INPUT);
+pinMode(A2,INPUT);
+pinMode(A3,INPUT);
+pinMode(A4,INPUT);
+pinMode(A5,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

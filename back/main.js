9d7690cd4ec74d6852681b04b875525f1717eb3d
lucho-girotti <onaicul2008@gmail.com>
2024-08-15 08:24:35 -0300

    se adaptó para usar json en vez de csv

--- back/main.js
+++ back/main.js
@@ -1,34 +1,29 @@
 const fs = require('fs');
-const csv = require('csv-parser');
+
+const horario = JSON.parse(fs.readFileSync('horario.json'));
+const ingles = JSON.parse(fs.readFileSync('ingles.json'));
 
 let date = new Date().getDay() - 1;
 const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
 let dia = diasSemana[date];
 
-let curso = 'B'
-let bloque = '1'
+let curso = 'B';
+let bloque = 3;
 
-fs.createReadStream('horario.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    if (row.dia === dia && row.curso === curso && row.bloque === bloque && row.aula === 'E'){
-      console.log('Tenés Ed. Física')
-    }
-    else if (row.dia === dia && row.curso === curso && row.bloque === bloque && row.aula === '0'){
-      console.log('No tenés ninguna materia en este bloque')
-    }
-    else if (row.dia === dia && row.curso === curso && row.bloque === bloque && row.aula === 'I'){
-      
-      fs.createReadStream('ingles.csv')
-      .pipe(csv())
-      .on('data', (row) => {
-        if (row.dia === dia && row.bloque === bloque){
-          console.log(`El nivel ${row.nivel} tiene en el aula ${row.aula}`)
+horario.forEach((row) => {
+  if (row.dia === dia && row.curso === curso && row.bloque === bloque) {
+    if (row.aula === 'E') {
+      console.log('Tenés Ed. Física');
+    } else if (row.aula === '0') {
+      console.log('No tenés ninguna materia en este bloque');
+    } else if (row.aula === 'I') {
+      ingles.forEach((inglesRow) => {
+        if (inglesRow.dia === dia && inglesRow.bloque === bloque) {
+          console.log(`El nivel ${inglesRow.nivel} tiene en el aula ${inglesRow.aula}`);
         }
-      }
-      )
-    }
-    else if (row.dia === dia && row.curso === curso && row.bloque === bloque) {
+      });
+    } else {
       console.log(`Tenés ${row.materia} en el Aula ${row.aula}`);
     }
-  })
+  }
+});

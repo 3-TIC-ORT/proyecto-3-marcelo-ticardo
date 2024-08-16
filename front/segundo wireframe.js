884600326e964e612f080c8e34d10d491b0ab6b2
lara0m <152939155+lara0m@users.noreply.github.com>
2024-08-16 15:51:36 -0300

    cambios en los botones
    cambiamos los botones hechos en js a html porque le convenía al backend

--- front/segundo wireframe.js
+++ front/segundo wireframe.js
@@ -1,64 +1,23 @@
-let container1 = document.getElementById("boton-bloque1");
-
-let button1 = document.createElement("button");
-button1.textContent = `Bloque 1`;
-container1.appendChild(button1);
-
 
 document.getElementById("boton-bloque1").addEventListener("click", function() {
     window.location.href = "tercer wireframe.html";
 });
 
-let container2 = document.getElementById("boton-bloque2");
-
-let button2 = document.createElement("button");
-button2.textContent = `Bloque 2`;
-container2.appendChild(button2);
-
-
 document.getElementById("boton-bloque2").addEventListener("click", function() {
     window.location.href = "tercer wireframe.html";
 });
-
-let container3 = document.getElementById("boton-bloque3");
-
-let button3 = document.createElement("button");
-button3.textContent = `Bloque 3`;
-container3.appendChild(button3);
-
 
 document.getElementById("boton-bloque3").addEventListener("click", function() {
     window.location.href = "tercer wireframe.html";
 });
 
-let container4 = document.getElementById("boton-bloque4");
-
-let button4 = document.createElement("button");
-button4.textContent = `Bloque 4`;
-container4.appendChild(button4);
-
-
 document.getElementById("boton-bloque4").addEventListener("click", function() {
     window.location.href = "tercer wireframe.html";
 });
 
-let container5 = document.getElementById("boton-bloque5")
-
-let button5 = document.createElement("button");
-button5.textContent = `Bloque 5`;
-container5.appendChild(button5);
-
-
 document.getElementById("boton-bloque5").addEventListener("click", function() {
     window.location.href = "tercer wireframe.html";
 });
-
-let container6 = document.getElementById("boton-bloque6")
-
-let button6 = document.createElement("button");
-button6.textContent = `Bloque 6`;
-container6.appendChild(button6);
-
 
 document.getElementById("boton-bloque6").addEventListener("click", function() {
     window.location.href = "tercer wireframe.html";

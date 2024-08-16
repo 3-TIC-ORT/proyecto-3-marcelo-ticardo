1600830c11ca5f6f632d532d87a8f1168dbc6956
Sabetay <lolasabetay@gmail.com>
2024-08-16 15:47:49 -0300

    .

--- hard/1/1.ino
+++ hard/1/1.ino
@@ -1,18 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
-pinMode(3,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(2,OUTPUT);
-pinMode(4,OUTPUT);
-pinMode(A0,INPUT);
-pinMode(A1,INPUT);
-pinMode(A2,INPUT);
-pinMode(A3,INPUT);
-pinMode(A4,INPUT);
-pinMode(A5,INPUT);
-}
+int IN1= 3;
+int IN2= 4;
 
-void loop() {
-  // put your main code here, to run repeatedly:
+ void setup (){
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+ }
 
-}
+ void loop (){
+  digitalWrite (IN1,HIGH);
+  digitalWrite (IN2,LOW);
+  delay (3000);
+  digitalWrite (IN1,LOW);
+  delay (3000);
+  
+ }

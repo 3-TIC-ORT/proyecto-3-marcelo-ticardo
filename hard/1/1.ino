b4f29681d7cf8ae7fee18241800f9f95c84e01ae
Sabetay <lolasabetay@gmail.com>
2024-08-28 15:54:40 -0300

    codigo  terminado

--- hard/1/1.ino
+++ hard/1/1.ino
@@ -1,42 +1,50 @@
-#define BUTTON 7
+#define BUTTONRIGHT 7
+#define BUTTONLEFT 8
 #define IN1 3
 #define IN2 4
 #define IN3 2
-#define IN4  5
+#define IN4 5
 #define ENA 11
 #define ENB 12
 
 int speedMotor = 255;
-bool statusButton
+bool statusLeft;
+bool statusRight;
  void setup (){
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
   pinMode(IN3,OUTPUT);
   pinMode(IN4,OUTPUT);
-  pinMode(BUTTON,INPUT);
+  pinMode(BUTTONRIGHT,INPUT);
+  pinMode(BUTTONLEFT,INPUT);
   pinMode(ENA,OUTPUT);
   pinMode(ENB,OUTPUT);
- }
-
- void loop (){
+  //encender motor
   digitalWrite (IN1,HIGH);
   digitalWrite (IN2,LOW);
   digitalWrite (IN3,HIGH);
   digitalWrite (IN4,LOW);
-  delay (3000);
-  digitalWrite (IN1,LOW);
-  digitalWrite (IN2,HIGH);
-  digitalWrite (IN3,LOW);
-  digitalWrite (IN4,HIGH);
-  delay (3000);
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
-  delay(3000);
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH);
-  delay(3000);
+ }
+
+ void loop (){
+  statusLeft = digitalRead (BUTTONLEFT);
+  statusRight = digitalRead (BUTTONRIGHT);
+
+  if(statusLeft == HIGH){
+    if(speedMotor <= 0){
+    speedMotor = 0;
+    } else {
+      speedMotor = speedMotor -5;
+    } 
+  }
+     if(speedMotor >= 255){
+      if(statusRight == HIGH){
+      speedMotor = 255;
+      } else {
+      speedMotor = speedMotor +5;
+      }
+   }
+   analogWrite(ENA,speedMotor);
+   analogWrite(ENB,speedMotor);
+   delay(150);
  }

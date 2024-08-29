d7faa9f945a0930d866e4c355b8ff9682af72f01
Sabetay <lolasabetay@gmail.com>
2024-08-29 08:43:22 -0300

    Update 1.ino

--- hard/1/1.ino
+++ hard/1/1.ino
@@ -1,4 +1,7 @@
-#define BUTTONRIGHT 7
+// Este codigo es para que al apretar un boton en la protoboard ambos motores aumenten la velocidad.
+//En cmabio si se apreta el otro motor que los motores vayan mas lento.
+
+#define BUTTONRIGHT 7  // definiendo cada variable con un pin digital
 #define BUTTONLEFT 8
 #define IN1 3
 #define IN2 4
@@ -11,7 +14,7 @@ int speedMotor = 255;
 bool statusLeft;
 bool statusRight;
  void setup (){
-  pinMode(IN1,OUTPUT);
+  pinMode(IN1,OUTPUT);  // definiendo si entra o sale info
   pinMode(IN2,OUTPUT);
   pinMode(IN3,OUTPUT);
   pinMode(IN4,OUTPUT);
@@ -19,7 +22,7 @@ bool statusRight;
   pinMode(BUTTONLEFT,INPUT);
   pinMode(ENA,OUTPUT);
   pinMode(ENB,OUTPUT);
-  //encender motor
+  //encender motores
   digitalWrite (IN1,HIGH);
   digitalWrite (IN2,LOW);
   digitalWrite (IN3,HIGH);
@@ -31,19 +34,20 @@ bool statusRight;
   statusRight = digitalRead (BUTTONRIGHT);
 
   if(statusLeft == HIGH){
-    if(speedMotor <= 0){
-    speedMotor = 0;
+    if(speedMotor <= 0){ // Si se apreta el boton de la derecha que la velociidad disminuya,
+    speedMotor = 0;      //si la velocidad del motor =< 0, que el motor frene.
     } else {
       speedMotor = speedMotor -5;
     } 
-  }
+  } 
+     
      if(speedMotor >= 255){
-      if(statusRight == HIGH){
-      speedMotor = 255;
+      if(statusRight == HIGH){ // Si se apreta el boton izquierdo que la velocidad aumente de 5 en 5.
+      speedMotor = 255;        // Si la velocidad de los motores <= 255, que se quede en 255
       } else {
       speedMotor = speedMotor +5;
       }
-   }
+   } 
    analogWrite(ENA,speedMotor);
    analogWrite(ENB,speedMotor);
    delay(150);

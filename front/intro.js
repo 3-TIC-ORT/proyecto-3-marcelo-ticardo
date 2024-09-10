81e32d0faad7d15ca5dc04911288ce839a7411fe
lara0m <152939155+lara0m@users.noreply.github.com>
2024-09-10 14:54:51 -0300

    boton para cambiar de modo oscuro a claro HECHO

--- front/intro.js
+++ front/intro.js
@@ -6,3 +6,12 @@ arrowButton.addEventListener('click', function() {
     // Cambia "otro_wireframe.html" al nombre de tu archivo o enlace
     window.location.href = 'cursos.html';
 });
+
+function thememode() {
+    let menu = document.getElementById("themeMode-check-container")
+    if (menu.classList.contains("themeMode-check-container-on")) {
+            menu.classList.remove("themeMode-check-container-on")
+    } else {
+            menu.classList.add("themeMode-check-container-on")
+    }
+}

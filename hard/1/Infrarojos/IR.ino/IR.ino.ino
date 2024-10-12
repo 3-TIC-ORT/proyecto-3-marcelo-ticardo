7d4b6fa29f8712c6a0ec1664f7adb869c1d1afb0
Sabetay <lolasabetay@gmail.com>
2024-10-11 22:52:17 -0300

    funciona

--- hard/1/Infrarojos/IR.ino/IR.ino.ino
+++ hard/1/Infrarojos/IR.ino/IR.ino.ino
@@ -1,4 +1,5 @@
-//al pasarle la mano por arriba al infrarrojo diga hola por puerto serie
+//al pasarle la mano por arriba al infrarrojo diga led prendiso por puerto serie y se ptrenda el led.
+// cuando sacar la mano se paga el led y por puerto serie dice led apagado
 
 #define IR 3
 #define LED 13
@@ -6,13 +7,16 @@ void setup() {
   pinMode(IR,INPUT);
   pinMode(LED,OUTPUT);
   digitalWrite(LED,LOW);
+  Serial.begin(9600);
 }
 
 void loop() {
   int estado = digitalRead(IR); 
- if(estado = LOW){
+ if(estado == HIGH){
   digitalWrite(LED,HIGH);
+  Serial.println("Led apagado");
   } else {
      digitalWrite(LED,LOW);
+     Serial.println("Led prendido");
     }
 }

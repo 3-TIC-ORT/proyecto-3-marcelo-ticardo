7b52b03fccc555d7925aeb35358151a5a81858b3
Sabetay <lolasabetay@gmail.com>
2024-10-14 11:55:47 -0300

    codigo gama de gris

--- hard/1/Infrarojos/Gama_Gris.ino/Gama_Gris/Gama_Gris.ino
+++ hard/1/Infrarojos/Gama_Gris.ino/Gama_Gris/Gama_Gris.ino
@@ -1,7 +1,18 @@
 #define IR A3
+unsigned long tiempoAnterior = 0;
+const long intervalo = 200;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
-  
-  
+  pinMode(IR, INPUT);
+}
+
+void loop() {
+  unsigned long tiempoActual = millis();  
+
+  if (tiempoActual - tiempoAnterior >= intervalo) {
+    tiempoAnterior = tiempoActual;  
+    int valorIR = analogRead(IR);  
+    Serial.println(valorIR);  S
   }
+}

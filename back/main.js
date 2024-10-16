1f544c599936bcb7702e60273d09f72fa599ab8c
GIROTTI LUCIANO <onaicul2008@gmail.com>
2024-10-16 11:32:00 -0300

    vamos bien con el mapa :)

--- back/main.js
+++ back/main.js
@@ -9,6 +9,7 @@ const ingles = JSON.parse(readFileSync('ingles.json'));
 let date = new Date().getDay() - 1;
 const diasSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'];
 const aulas = ['inicio', 'L202', 'L204', 'L206', 'L208', 'L4', 'L3', 'L2', 'L1', 'L213', 'L215', 'L217', 'L218', 'L216', 'L207', 'L205', 'L203', 'L201', 'L200'];
+const aluas_inv = [aulas[0], ...aulas.slice(-7).reverse()]
 let dia = diasSemana[date]
 let curso
 let bloque
@@ -73,7 +74,7 @@ onEvent("mapa", ()=>{
   if (direccion = 'ADELANTE'){
     distancia = aulas.indexOf(objetivo)
   } else{
-    distancia = [aulas[0], ...aulas.slice(-7).reverse()].indexOf(objetivo)
+    distancia = aluas_inv.indexOf(objetivo)
   }
   arduino()
   return [objetivo, direccion]
---
+++ mapa/mapa.js
@@ -0,0 +1,6 @@
+const aulas = ['inicio', 'L202', 'L204', 'L206', 'L208', 'L4', 'L3', 'L2', 'L1', 'L213', 'L215', 'L217', 'L218', 'L216', 'L207', 'L205', 'L203', 'L201', 'L200'];
+const aluas_inv = [aulas[0], ...aulas.slice(-7).reverse()]  
+
+document.getElementById("L200").setAttribute("visibility", "visible")
+document.getElementById("L201").setAttribute("visibility", "visible")
+document.getElementById("inicio_u").setAttribute("visibility", "visible")

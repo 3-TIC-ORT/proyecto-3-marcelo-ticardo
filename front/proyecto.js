71de7e0cd9ca1b914a12fc8572f028cf3483793c
lara0m <152939155+lara0m@users.noreply.github.com>
2024-10-23 10:43:57 -0300

    logo gif agregado

--- front/proyecto.js
+++ front/proyecto.js
@@ -3,8 +3,7 @@ localStorage.setItem('theme', '');
 function thememode() {
     let menu = document.getElementById("themeMode-check-container");
     let body = document.getElementById("body");
-    let boton = document.getElementById("contenedor");   // Contenedor del GIF claro
-    let boton1 = document.getElementById("contenedor1"); // Contenedor del GIF oscuro
+    let boton = document.getElementById("logo-gif");   // Contenedor del GIF claro
     let solIcon = document.getElementById("sol-icon");
     let lunaIcon = document.getElementById("luna-icon");
 
@@ -17,10 +16,6 @@ function thememode() {
         // Cambiar el fondo a blanco para el modo claro
         body.style.backgroundColor = "white";
         menu.style.backgroundColor = "white";
-
-        // Ajustar z-index: contenedor claro arriba
-        boton.style.zIndex = "1";    // Contenedor claro tiene prioridad
-        boton1.style.zIndex = "-1";  // Contenedor oscuro detrás
 
         // Cambiar los íconos
         solIcon.src = "sol-claro.svg";
@@ -36,11 +31,7 @@ function thememode() {
         // Cambiar el fondo a oscuro
         body.style.backgroundColor = '#2C3148';
         menu.style.backgroundColor = '#2C3148';
-
-        // Ajustar z-index: contenedor oscuro arriba
-        boton.style.zIndex = "1";   // Contenedor claro detrás
-        boton1.style.zIndex = "2";   // Contenedor oscuro tiene prioridad
-
+        boton.src = "Logo-1-[remix]-oscuro.gif"
         // Cambiar los íconos
         solIcon.src = "sol-oscuro.svg";
         lunaIcon.src = "luna-oscuro.svg";
@@ -48,6 +39,7 @@ function thememode() {
         localStorage.setItem('theme', '-oscuro');
     }
 }
+
 
 
 document.getElementById("boton-inicio").addEventListener("click", function() {

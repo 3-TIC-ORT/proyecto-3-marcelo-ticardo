7ea97acee984ab94b961234ee5d03c589b6aa77d
Sabetay <lolasabetay@gmail.com>
2024-11-21 15:59:25 -0300

    cambie todo el codigo de IIR
    logicca que hay que probar

--- hard/1/Infrarojos/IR.ino/IR.ino.ino
+++ hard/1/Infrarojos/IR.ino/IR.ino.ino
@@ -1,5 +1,5 @@
-#define ENA 10  
-#define ENB 11  
+#define EN1 10  
+#define EN2 11  
 #define IN1 3   
 #define IN2 4  
 #define IN3 2   
@@ -9,22 +9,22 @@
 #define IR3 A4 
 #define IR4 A5
 
-
 unsigned long tiempoAnterior = 0;
+unsigned long tiempoRetroceso = 0;
+unsigned long tiempoAvance = 0;
+
 const long intervalo = 200;
 int velocidadmax = 255;
-int velocidadCorreccion = 50;
+int velocidadCorreccion = 177;
 int velocidadmin = 0;
-
-
 
 void setup() {
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
+  pinMode(EN1, OUTPUT);
+  pinMode(EN2, OUTPUT);
   pinMode(IR1, INPUT);
   pinMode(IR2, INPUT);
   pinMode(IR3, INPUT);
@@ -33,7 +33,6 @@ void setup() {
 }
 
 void loop() {
-  
   unsigned long tiempoActual = millis();
   if (tiempoActual - tiempoAnterior >= intervalo) {
     tiempoAnterior = tiempoActual;
@@ -44,12 +43,38 @@ void loop() {
   int valorIR3 = analogRead(IR3);  
   int valorIR4 = analogRead(IR4);  
 
-  if (valorIR1 < 100 && valorIR3 < 100 && valorIR2 > 500 && valorIR4 > 500) {
-      moverAdelante();
-  } else if (valorIR1 > 100 && valorIR3 > 100) {
-    correccionDerecha();
-  } else if (valorIR4 < 500 && valorIR2 < 500) {
-    correccionIzquierda();
+  // Caso 1: IR1 e IR3 > 500
+  if (valorIR1 > 500 && valorIR3 > 500) {
+    detenerMotor(EN2);
+    retrocederMotor1();
+
+    while (analogRead(IR1) > 100 || analogRead(IR3) > 100) {
+      delay(10); 
+    }
+
+    tiempoRetroceso = millis() - tiempoAnterior;
+    tiempoAvance = tiempoRetroceso / 2;
+    
+    avanzarMotor1(tiempoAvance);
+  }
+
+  // Caso 2: IR2 e IR4 < 100
+  else if (valorIR2 < 100 && valorIR4 < 100) {
+    detenerMotor(EN1);
+    retrocederMotor2();
+
+
+    while (analogRead(IR2) < 500 || analogRead(IR4) < 500) {
+      delay(10); 
+    }
+
+
+    tiempoRetroceso = millis() - tiempoAnterior;
+    tiempoAvance = tiempoRetroceso / 2;
+
+    avanzarMotor2(tiempoAvance);
+  } else {
+    moverAdelante();
   }
 }
 
@@ -58,8 +83,40 @@ void detenerRobot() {
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, velocidadmin);
-  analogWrite(ENB, velocidadmin);
+  analogWrite(EN1, velocidadmin);
+  analogWrite(EN2, velocidadmin);
+}
+
+void detenerMotor(int motor) {
+  analogWrite(motor, velocidadmin);
+}
+
+void retrocederMotor1() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(EN1, velocidadmax);
+}
+
+void retrocederMotor2() {
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(EN2, velocidadmax);
+}
+
+void avanzarMotor1(unsigned long tiempo) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(EN1, velocidadmax);
+  delay(tiempo);
+  detenerMotor(EN1);
+}
+
+void avanzarMotor2(unsigned long tiempo) {
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(EN2, velocidadmax);
+  delay(tiempo);
+  detenerMotor(EN2);
 }
 
 void moverAdelante() {
@@ -67,34 +124,6 @@ void moverAdelante() {
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, velocidadmax);
-  analogWrite(ENB, velocidadmax);
+  analogWrite(EN1, velocidadmax);
+  analogWrite(EN2, velocidadmax);
 }
-
-void moverAtras() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(ENA, velocidadmax);
-  analogWrite(ENB, velocidadmax);
-}
-
-void correccionIzquierda() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, velocidadCorreccion);
-  analogWrite(ENB, velocidadmax);
-}
-
-void correccionDerecha() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, velocidadmax);
-  analogWrite(ENB, velocidadCorreccion);
-}
-  

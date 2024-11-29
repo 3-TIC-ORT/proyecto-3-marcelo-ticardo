943614fffb0860f281b8cb7032b20b45281f0967
Sabetay <lolasabetay@gmail.com>
2024-11-29 16:00:07 -0300

    ns

--- hard/1/Infrarojos/IR_Prueba.ino/IR_Prueba.ino.ino
+++ hard/1/Infrarojos/IR_Prueba.ino/IR_Prueba.ino.ino
@@ -1,7 +1,7 @@
-#define EN1 10  
-#define EN2 11  
-#define IN1 3  //mor izq 
-#define IN2 4  //mor izq
+#define EN1 10  //mot izq
+#define EN2 11  //mot der
+#define IN1 3  //mot izq 
+#define IN2 4  //mot izq
 #define IN3 2  //mot der
 #define IN4 5  //mot der
 #define IR1 A2 //der
@@ -14,8 +14,10 @@ unsigned long tiempoRetroceso = 0;
 unsigned long tiempoAvance = 0;
 
 const long intervalo = 200;
-int velocidadmax = 255;
-int velocidadCorreccion = 177;
+//int velocidadmax = 255;
+//int velocidadCorreccion = 177;
+int velocidadmax = 177;
+int velocidadCorreccion = 150;
 int velocidadmin = 0;
 
 void setup() {
@@ -43,16 +45,16 @@ void loop() {
   int valorIR3 = analogRead(IR3);  
   int valorIR4 = analogRead(IR4);  
 
-  Serial.print("IR1: "); Serial.println(valorIR1); //der
-  Serial.print("IR2: "); Serial.println(valorIR2); //izq
-  Serial.print("IR3: "); Serial.println(valorIR3); //der
-  Serial.print("IR4: "); Serial.println(valorIR4); //izq
+//  Serial.print("IR1: "); Serial.println(valorIR1); //der
+//  Serial.print("IR2: "); Serial.println(valorIR2); //izq
+//  Serial.print("IR3: "); Serial.println(valorIR3); //der
+//  Serial.print("IR4: "); Serial.println(valorIR4); //izq
 
   // Caso 1: IR1 e IR3 > 800
   if (valorIR1 > 800 && valorIR3 > 800) { //Si mis sensores de la der no ven blanco
   detenerMotor(EN2);
   detenerMotor(EN1);
-  retrocederMotor1(velocidadCorreccion); //Enciendo el motor izquierdo para
+  retrocederMotor2(velocidadCorreccion); //Enciendo el motor izq para atras
   
   // Actualiza tiempoAnterior antes del while
   tiempoAnterior = millis();
@@ -65,34 +67,48 @@ void loop() {
     Serial.print("IR1: "); Serial.println(valorIR1); //der
     Serial.print("IR2: "); Serial.println(valorIR2); //izq
     Serial.print("IR3: "); Serial.println(valorIR3); //der
-    Serial.print("IR4: "); Serial.println(valorIR4); //izq
+    Serial.print("IR4: "); Serial.println(valorIR4); //izq}
+    if(millis() - tiempoAnterior > 2500){
+      retrocederMotor1(velocidadCorreccion);
+      delay(1000);
+      break;
+    }
   }
-  Serial.print("corrigiendo motor otro, tiempo retoseso ");
+  Serial.print("corrigiendo motor otro, tiempo retoceso ");
+  detenerMotor(EN2);
   detenerMotor(EN1);
   tiempoRetroceso = millis() - tiempoAnterior;
-  Serial.print(tiempoRetroceso);
-  Serial.print(" tiempo Avance ");
-  tiempoAvance = tiempoRetroceso / 2;
-  Serial.println(tiempoAvance);
-  avanzarMotor1(tiempoAvance, velocidadCorreccion);
-  
-}
+  if (valorIR2 < 100 && valorIR4 < 100)
+    Serial.println("Sigue descal");
 
+}
   // Caso 2: IR2 e IR4 < 100
   else if (valorIR2 < 100 && valorIR4 < 100) {
     detenerMotor(EN1);
-    retrocederMotor2(velocidadCorreccion);
+    detenerMotor(EN2);
+    retrocederMotor1(velocidadCorreccion);
+    
+  // Actualiza tiempoAnterior antes del while
+  tiempoAnterior = millis();
     Serial.println("corrigiendo no veo NEGRO");
     while (analogRead(IR2) < 800 || analogRead(IR4) < 800) {
       delay(10); // Espera hasta que ambos sensores detecten valores > 800
+      valorIR1 = analogRead(IR1);  
+      valorIR3 = analogRead(IR3); 
+      Serial.println("corrigiendo no veo blanco");
+      Serial.print("IR1: "); Serial.println(valorIR1); //der
+      Serial.print("IR2: "); Serial.println(valorIR2); //izq
+      Serial.print("IR3: "); Serial.println(valorIR3); //der
+      Serial.print("IR4: "); Serial.println(valorIR4); //izq
+      if(millis() - tiempoAnterior > 2500){
+      retrocederMotor2(velocidadCorreccion);
+      delay(1000);
+      break;
     }
-
+    }
+    detenerMotor(EN1);
     detenerMotor(EN2);
-    tiempoRetroceso = millis() - tiempoAnterior;
-    tiempoAvance = tiempoRetroceso / 2;
-    avanzarMotor2(tiempoAvance, velocidadCorreccion);
   }
-
   // Movimiento por defecto: avanzar
   else {
     moverAdelante();

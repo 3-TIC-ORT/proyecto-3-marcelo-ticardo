8369f77297772d704c5a05ba5f593f80ca3463ba
Sabetay <lolasabetay@gmail.com>
2025-06-04 23:10:33 -0300

    correcion capatciones.ino
    Le agregue comentarios y me remarque lo que tengo que cambiar

--- hard/Camptaciones/Camptaciones.ino
+++ hard/Camptaciones/Camptaciones.ino
@@ -2,8 +2,8 @@
 // y ahora la pista va a ser una franja negra en un fondo balco, los 4 sensores tienen q sensar negro
 // Si alguno sensa blanco arranca a buscar la linea.
 // LINK A CHAT: https://chatgpt.com/c/6840d294-f654-800c-b4aa-df2b874bc869
-#define ENA 10  
-#define ENB 11  
+#define ENA 10  //motor
+#define ENB 11  //motor
 #define IN1 3   
 #define IN2 4  
 #define IN3 2   
@@ -13,16 +13,16 @@
 #define TRIG2 8
 #define ECHO2 A0   
 
-#define IR1 A2 // sensor1 (gris)
-#define IR2 A3 // sensor2 (negro)
-#define IR3 A4 // sensor3 (gris)
-#define IR4 A5 // sensor4 (negro)
+#define IR1 A2 // sensor1 
+#define IR2 A3 // sensor2 
+#define IR3 A4 // sensor3 
+#define IR4 A5 // sensor4 
 
 #define SOUND_SPEED 0.034 
 #define DISTANCIA_MINIMA 75
 #define INTERVALO 200
 
-int velocidadena = 255;
+int velocidadena = 255; // ESTOS 2 LOS TENGO QUE CAMBIAR PQ LOS MOTORES SEGURO SON DIFERENTES
 int velocidadenb = 65;
 int velocidadCorreccion = 90;
 int velocidadmin = 0;
@@ -30,23 +30,16 @@ int velocidadmin = 0;
 unsigned long medicionAnterior = 0;
 bool direccion = true; // true = adelante, false = atrás
 
-int umbralLinea = 700; // Ajustar según la calibración de los sensores
+int umbralLinea = 700; // TENGO QUE PONER EL VALOR DEL NEGRO
 
 void setup() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  pinMode(TRIG1, OUTPUT);
-  pinMode(ECHO1, INPUT);
-  pinMode(TRIG2, OUTPUT);
-  pinMode(ECHO2, INPUT);
-  pinMode(IR1, INPUT);
-  pinMode(IR2, INPUT);
-  pinMode(IR3, INPUT);
-  pinMode(IR4, INPUT);
+  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT); pinMode(ENB, OUTPUT);
+  pinMode(TRIG1, OUTPUT); pinMode(ECHO1, INPUT);
+  pinMode(TRIG2, OUTPUT); pinMode(ECHO2, INPUT);
+  pinMode(IR1, INPUT); pinMode(IR2, INPUT);
+  pinMode(IR3, INPUT); pinMode(IR4, INPUT);
 
   detenerRobot();
 }
@@ -54,10 +47,9 @@ void setup() {
 void loop() {
   seguirLinea();
 
-  if (millis() - medicionAnterior >= INTERVALO) {
+  if (millis() - medicionAnterior >= INTERVALO) { // si ya paso el tiempo de intervalo q vuelva a corre el if
     medicionAnterior = millis();
-    float distancia = medirDistancia(direccion ? TRIG1 : TRIG2, direccion ? ECHO1 : ECHO2);
-
+    float distancia = medirDistancia(direccion ? TRIG1 : TRIG2, direccion ? ECHO1 : ECHO2); // Si direccion es true, usa TRIG1. Si direccion es false, usa TRIG2.
     if (distancia <= DISTANCIA_MINIMA) {
       detenerRobot();
     }

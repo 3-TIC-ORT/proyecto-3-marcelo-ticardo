f1a1f1815217fb9bf659866268b311a3443937e9
Sabetay <lolasabetay@gmail.com>
2025-06-09 10:37:16 -0300

    motor izq no funciona

--- hard/1/prueba_de_motores/prueba_de_motores.ino
+++ hard/1/prueba_de_motores/prueba_de_motores.ino
@@ -4,9 +4,8 @@
 #define IN2 4  
 #define IN3 2  
 #define IN4 5  
- 
-int velocidad = 255;   
 
+int velocidad = 255;   
 
 void setup() {
   pinMode(IN1, OUTPUT);
@@ -15,6 +14,8 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
+  
+  Serial.begin(9600);  
 }
 
 void loop() {
@@ -24,12 +25,15 @@ void loop() {
   digitalWrite(IN4, LOW);
   analogWrite(ENA, velocidad); 
   analogWrite(ENB, velocidad);
+  Serial.println("Moviendo hacia adelante");
   delay(5000);
+  
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
   analogWrite(ENA, velocidad);
   analogWrite(ENB, velocidad);
+  Serial.println("Moviendo hacia atras");
   delay(5000);
 }

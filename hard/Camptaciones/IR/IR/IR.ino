6a93e24a05fb36ebf317a6361bfc4cad0f465d8b
Sabetay <lolasabetay@gmail.com>
2025-06-12 08:55:17 -0300

    muchos cambios
    Hice un nuevo codigo IR
    Corregi el codigo de Camptaciones
    Tengo que cambiar algo enn los cables de los motores

--- hard/Camptaciones/IR/IR/IR.ino
+++ hard/Camptaciones/IR/IR/IR.ino
@@ -6,30 +6,16 @@
 #define IN3 2   // motor derecho
 #define IN4 5
 
-// Ultrasonicos
-#define TRIG1 7
-#define ECHO1 A0
-#define TRIG2 8
-#define ECHO2 A1
-#define INTERVALO 100
-#define SOUND_SPEED 0.034
-#define DISTANCIA_MINIMA 75
-
 // Infrarrojos
 #define IR1 A2 // sensor derecha delantera
 #define IR2 A3 // sensor izquierda delantera
 #define IR3 A4 // sensor derecha trasera
 #define IR4 A5 // sensor izquierda trasera
 
-int velocidadIzquierda = 255;   // Lo tengo que corregir
-int velocidadDerecha = 65;      // Lo tengo que corregir
+int velocidadIzquierda = 255;   //Corregir con las velocidades de los motores
+int velocidadDerecha = 65;      
 int velocidadCorreccion = 90;
-int velocidadMax = 255;
 int velocidadMinima = 0;
-
-unsigned long UltimaMedicion = 0;
-bool direccion = true;
-bool obstaculoDetectado = false;
 
 void setup() {
   pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
@@ -37,45 +23,15 @@ void setup() {
   pinMode(ENA, OUTPUT); pinMode(ENB, OUTPUT);
   pinMode(IR1, INPUT); pinMode(IR2, INPUT);
   pinMode(IR3, INPUT); pinMode(IR4, INPUT);
-  pinMode(TRIG1, OUTPUT); pinMode(ECHO1, INPUT);
-  pinMode(TRIG2, OUTPUT); pinMode(ECHO2, INPUT);
+
 
   Serial.begin(9600);  
   detenerRobot();
 }
 
 void loop() {
-  if (!obstaculoDetectado) {
-    direccion ? moverAdelante() : moverAtras();
-  }
-
-  if (millis() - UltimaMedicion >= INTERVALO) {
-    UltimaMedicion = millis();
-    float distancia = medirDistancia(direccion ? TRIG1 : TRIG2, direccion ? ECHO1 : ECHO2);
-
-    Serial.print(direccion ? "Distancia Sensor Frontal: " : "Distancia Sensor Trasero: ");
-    Serial.print(distancia);
-    Serial.println(" cm");
-
-    if (distancia <= DISTANCIA_MINIMA) {
-      detenerRobot();
-      obstaculoDetectado = true;
-    } else if (obstaculoDetectado) {
-      obstaculoDetectado = false;
-    }
-  }
 
   seguirLinea();
-}
-
-float medirDistancia(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  long duracion = pulseIn(echoPin, HIGH);
-  return duracion * SOUND_SPEED / 2;
 }
 
 void seguirLinea() {
